b0168a0ed00107af511826364afb869757c621d4
Lazylactus <rpitfootsteps@gmail.com>
2020-11-23 21:34:03 +0530

    Add conditions

--- app.js
+++ app.js
@@ -13,8 +13,15 @@ app.get('/', (request, response) => {
   response.send('Hello World');
 });
 
-app.post('/mychef', function (req, res) {
-  res.json({ requestBody: req.body });
+app.post('/mychef', function (request, response) {
+  const data = request.body.text;
+  if (data === 'hello' || data === 'Hello') {
+    response.json('Hello my friend');
+  } else if (data === 'breakfast') {
+    response.json('Would like to have some cereals');
+  } else {
+    response.json('Hohoho how can I help mate?');
+  }
 });
 
 app.listen(port, () => {
