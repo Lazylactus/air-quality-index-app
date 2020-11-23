da0af4430a8b8f553817850e947adef63c4440f8
Lazylactus <rpitfootsteps@gmail.com>
2020-11-23 18:13:56 +0530

    Changes

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const app = express();
-const 
-
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
 
 app.use(
   express.urlencoded({
-    extended: true
+    extended: true,
   })
-)
+);
+app.use(express.json());
 
 app.get('/', (request, response) => {
   response.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
 app.post('/mychef', function (req, res) {
   res.json({ requestBody: req.body });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
