b185f0e9e8fc3968c818e21052c19d8925014026
Lazylactus <rpitfootsteps@gmail.com>
2020-11-26 01:53:40 +0530

    Test3

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ app.get('/', (request, response) => {
 
 app.post('/mychef', function (request, response) {
   const data = request.body.text;
-  if (data === 'hello' || data === 'Hello' || 'intro') {
-    response.send(
-      'Hello stranger, I am an AQI(Air quality index) collector. I collect AQIs of places around the global and you can call me anytime to know about em'
-    );
-  } else {
-    handleMessage(data);
-  }
+  // if (data === 'hello' || data === 'Hello' || 'intro') {
+  //   response.send(
+  //     'Hello stranger, I am an AQI(Air quality index) collector. I collect AQIs of places around the global and you can call me anytime to know about em'
+  //   );
+  // } else {
+  handleMessage(data);
+  //}
 });
 
 // RESPONSE TO DATA
@@ -33,7 +33,7 @@ function handleMessage(message) {
   // message = message[1];
   app.get(
     `http://api.waqi.info/feed/${message}/?token=82033d0b4fc868607cc0dc55567b8ecc4bac9822`,
-    () => {
+    (request, response) => {
       response.send(data.api);
     }
   );
