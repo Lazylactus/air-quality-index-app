710084b76eeecc3b6838772cc7069dc673362a0a
Lazylactus <rpitfootsteps@gmail.com>
2020-11-26 01:26:51 +0530

    Add API

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const { response } = require('express');
 const express = require('express');
+const imdb = require('imdb-api');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,19 +12,40 @@ app.use(
 app.use(express.json());
 
 app.get('/', (request, response) => {
-  response.send('Hello World');
+  response.send('Hello Stranger!');
 });
+
+// ROUTE HANDLER
 
 app.post('/mychef', function (request, response) {
   const data = request.body.text;
-  if (data === 'hello' || data === 'Hello') {
-    response.send('Hello my friend');
-  } else if (data === 'breakfast') {
-    response.send('Would like to have some cereals');
-  } else {
-    response.send('Hohoho how can I help mate?');
+  if (data === 'hello' || data === 'Hello' || 'intro') {
+    response.send(
+      'Hello stranger, I am an AQI(Air quality index) collector. I collect AQIs of places around the global and you can call me anytime to know about em'
+    );
+  } else if (data === 'city') {
+    response.send('Type the city name');
+    app.on('message', (data) => {
+      if (data.type !== 'message') return;
+
+      handleMessage(data.text);
+    });
   }
 });
+
+// RESPONSE TO DATA
+function handleMessage(message) {
+  message = message.split(' ');
+  app.get(
+    `http://api.waqi.info/feed/${message}/?token=82033d0b4fc868607cc0dc55567b8ecc4bac9822`,
+    () => {
+      response.send(data.api);
+    }
+  );
+}
+
+// ERROR HANDLER
+app.on('error', (err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
