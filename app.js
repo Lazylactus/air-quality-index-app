2968048ab8724a37050e92962ddbe44f8496e999
Lazylactus <rpitfootsteps@gmail.com>
2020-11-26 14:37:52 +0530

    Test 16

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.post('/mychef', function (request, response) {
 
 // RESPONSE TO DATA
 function handleMessage(message) {
+  console.log('handle ', message);
   let result;
   app.get(
     `http://api.waqi.info/feed/${message}/?token=82033d0b4fc868607cc0dc55567b8ecc4bac9822`,
