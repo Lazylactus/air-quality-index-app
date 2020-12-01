8cd0bf4f5dd9a641abcb36cb24e25122218fedec
Lazylactus <rpitfootsteps@gmail.com>
2020-12-01 10:31:13 +0530

    Add Get request

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(
 );
 app.use(express.json());
 
+app.get('/', (request, response) => {
+  response.send('Hello Stranger!');
+});
+
 // POST HANDLER
 
 app.post('/mychef', async function (request, response) {
